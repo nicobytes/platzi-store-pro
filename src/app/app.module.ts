203819f446e8b44227d419a1e4a807d21b8d5402
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-23 14:35:59 -0400

    🚧 work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { environment } from './../environments/environment';
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
